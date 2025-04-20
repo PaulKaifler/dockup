246d08d96d8c063c65e569a2e11b8da198ed7fdf
Paul Kaifler <paul@kaifler.eu>
2025-04-20 13:24:48 +0200

    Iter

--- src/restore.rs
+++ src/restore.rs
@@ -68,11 +68,14 @@ fn enter_interactive_shell(config: &Config) -> io::Result<()> {
 
 pub struct RestoreApp {
     backups: Vec<BackupApplication>,
+    projects: Vec<String>,
     config: RestoreConfig,
     exit: bool,
-    selected_index: usize,
-    selected_volumes: HashSet<String>,
+    selected_project_index: usize,
+    selected_backup_index: usize,
+    selected_volume_index: usize,
     selected_column: usize,
+    selected_volumes: HashSet<String>,
 }
 
 impl RestoreApp {
@@ -81,13 +84,24 @@ impl RestoreApp {
             eprintln!("❌ Error scanning backup target: {e}");
             Vec::new()
         });
+
+        let mut seen = HashSet::new();
+        let projects: Vec<String> = backups
+            .iter()
+            .map(|b| b.name.clone())
+            .filter(|name| seen.insert(name.clone()))
+            .collect();
+
         Self {
             backups,
+            projects,
             config: RestoreConfig::emppty(),
             exit: false,
-            selected_index: 0,
-            selected_volumes: HashSet::new(),
+            selected_project_index: 0,
+            selected_backup_index: 0,
+            selected_volume_index: 0,
             selected_column: 0,
+            selected_volumes: HashSet::new(),
         }
     }
 }
@@ -120,16 +134,16 @@ impl RestoreApp {
 
         // Render projects
         let project_names: Vec<Line> = self
-            .backups
+            .projects
             .iter()
             .enumerate()
             .map(|(i, app)| {
-                let style = if self.selected_column == 0 && i == self.selected_index {
+                let style = if self.selected_column == 0 && i == self.selected_project_index {
                     Style::default().add_modifier(ratatui::style::Modifier::REVERSED)
                 } else {
                     Style::default()
                 };
-                Line::from(app.name.clone()).style(style)
+                Line::from(app.clone()).style(style)
             })
             .collect();
         Paragraph::new(Text::from(project_names))
@@ -141,12 +155,14 @@ impl RestoreApp {
             .render(chunk[0], frame.buffer_mut());
 
         // Render dates
+        let selected_project = &self.projects[self.selected_project_index];
         let dates: Vec<Line> = self
             .backups
             .iter()
+            .filter(|b| &b.name == selected_project)
             .enumerate()
             .map(|(i, app)| {
-                let style = if self.selected_column == 1 && i == self.selected_index {
+                let style = if self.selected_column == 1 && i == self.selected_backup_index {
                     Style::default().add_modifier(ratatui::style::Modifier::REVERSED)
                 } else {
                     Style::default()
@@ -163,25 +179,36 @@ impl RestoreApp {
             .render(chunk[1], frame.buffer_mut());
 
         // Render volume checkboxes
-        let volume_texts: Vec<Line> = self
-            .config
-            .volumes
+        let selected_backups: Vec<_> = self
+            .backups
             .iter()
-            .enumerate()
-            .map(|(i, volume)| {
-                let style = if self.selected_column == 2 && i == self.selected_index {
-                    Style::default().add_modifier(ratatui::style::Modifier::REVERSED)
-                } else {
-                    Style::default()
-                };
-                let checkbox: String = if self.selected_volumes.contains(&volume.name) {
-                    "[x] ".to_string()
-                } else {
-                    "[ ] ".to_string()
-                };
-                Line::from(format!("{}{}", checkbox, volume.name)).style(style)
-            })
+            .filter(|b| &b.name == selected_project)
             .collect();
+        let volume_texts: Vec<Line> = if let Some(selected_backup) =
+            selected_backups.get(self.selected_backup_index)
+        {
+            selected_backup
+                .volumes
+                .iter()
+                .enumerate()
+                .map(|(i, volume)| {
+                    let style = if self.selected_column == 2 && i == self.selected_volume_index {
+                        Style::default().add_modifier(ratatui::style::Modifier::REVERSED)
+                    } else {
+                        Style::default()
+                    };
+                    let checkbox: String = if self.selected_volumes.contains(&volume.name) {
+                        "[x] ".to_string()
+                    } else {
+                        "[ ] ".to_string()
+                    };
+                    Line::from(format!("{}{}", checkbox, volume.name)).style(style)
+                })
+                .collect()
+        } else {
+            Vec::new()
+        };
+
         Paragraph::new(Text::from(volume_texts))
             .block(
                 Block::default()
@@ -192,9 +219,9 @@ impl RestoreApp {
 
         // Render summary
         let summary_text = format!(
-            "Selected Project: {}\nSelected Volumes: {:?}",
+            "Selected Project: {}\n Selected Volumes: {:?}",
             self.backups
-                .get(self.selected_index)
+                .get(self.selected_backup_index)
                 .map_or("None".to_string(), |app| app.name.clone()),
             self.selected_volumes
         );
@@ -220,41 +247,68 @@ impl RestoreApp {
     fn handle_key_event(&mut self, key_event: KeyEvent) {
         match key_event.code {
             KeyCode::Up => {
-                if self.selected_column == 0 && self.selected_index > 0 {
-                    self.selected_index -= 1;
-                } else if self.selected_column == 1 && self.selected_index > 0 {
-                    self.selected_index -= 1;
-                } else if self.selected_column == 2 && self.selected_index > 0 {
-                    self.selected_index -= 1;
+                if self.selected_column == 0 && self.selected_project_index > 0 {
+                    self.selected_project_index -= 1;
+                } else if self.selected_column == 1 {
+                    let selected_project = &self.projects[self.selected_project_index];
+                    let matching_backups: Vec<_> = self
+                        .backups
+                        .iter()
+                        .filter(|b| &b.name == selected_project)
+                        .collect();
+                    if self.selected_backup_index > 0 && !matching_backups.is_empty() {
+                        self.selected_backup_index -= 1;
+                    }
+                } else if self.selected_column == 2 {
+                    let selected_backups: Vec<_> = self
+                        .backups
+                        .iter()
+                        .filter(|b| &b.name == &self.projects[self.selected_project_index])
+                        .collect();
+                    if self.selected_volume_index > 0 && !selected_backups.is_empty() {
+                        self.selected_volume_index -= 1;
+                    }
                 }
             }
             KeyCode::Down => {
-                if self.selected_column == 0 && self.selected_index < self.backups.len() - 1 {
-                    self.selected_index += 1;
-                } else if self.selected_column == 1 && self.selected_index < self.backups.len() - 1
+                if self.selected_column == 0
+                    && self.selected_project_index < self.projects.len() - 1
                 {
-                    self.selected_index += 1;
-                } else if self.selected_column == 2
-                    && self.selected_index < self.config.volumes.len() - 1
-                {
-                    self.selected_index += 1;
+                    self.selected_project_index += 1;
+                } else if self.selected_column == 1 {
+                    let selected_project = &self.projects[self.selected_project_index];
+                    let matching_backups: Vec<_> = self
+                        .backups
+                        .iter()
+                        .filter(|b| &b.name == selected_project)
+                        .collect();
+                    if self.selected_backup_index < matching_backups.len() - 1 {
+                        self.selected_backup_index += 1;
+                    }
+                } else if self.selected_column == 2 {
+                    let selected_backups: Vec<_> = self
+                        .backups
+                        .iter()
+                        .filter(|b| &b.name == &self.projects[self.selected_project_index])
+                        .collect();
+                    if self.selected_volume_index < selected_backups.len() - 1 {
+                        self.selected_volume_index += 1;
+                    }
                 }
             }
             KeyCode::Left => {
                 if self.selected_column > 0 {
                     self.selected_column -= 1;
-                    self.selected_index = 0;
                 }
             }
             KeyCode::Right => {
                 if self.selected_column < 2 {
                     self.selected_column += 1;
-                    self.selected_index = 0;
                 }
             }
             KeyCode::Char(' ') => {
                 if self.selected_column == 2 {
-                    let selected_backup = &self.backups[self.selected_index];
+                    let selected_backup = &self.backups[self.selected_backup_index];
                     if self.selected_volumes.contains(&selected_backup.name) {
                         self.selected_volumes.remove(&selected_backup.name);
                     } else {
